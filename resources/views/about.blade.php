3e73fed35fcaaead478b9cd20292c6d5d8ab7bf1
MasaidFairus <masaidfairustrimarsongko@gmail.com>
2025-03-17 13:54:44 +0700

    2. laravel blade templating

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
 
 @section('content')
-  <h1 class="text-3xl">Ini Halaman {{ $title }}</h1>
+    <h1 class="text-3xl">Ini Halaman {{ $title }}</h1>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('homepage');
+    return view('homepage', ['title' => 'Homepage']);
+});
+
+Route::get('/hall', function () {
+    return view('hall', ['title' => 'Hall']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About']);
 });
